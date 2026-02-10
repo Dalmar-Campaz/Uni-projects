efb9f5e65a845e24b5280714f30ad11a292afa62
Dalmar-Campaz <dgcampaz@estudiante.uniajc.edu.co>
2026-02-10 16:26:53 +0000

    Primer ejercicio con arrays

---
+++ Semana 2/ejercicio1.java
@@ -0,0 +1,10 @@
+public class ejercicio1 {
+    public static void main(String[] args) {
+        //1st form: definiendo valores
+        int[] v = {1,4,5,7,9,2};
+        //impresion
+        for (int i = 0; i < v.length; i++){
+            System.out.println("V["+i+"]= "+ v[i]);
+        }
+    }
+}

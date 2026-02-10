817f2b96d85be1b85b1269e7666dd319d6ebd8c0
Dalmar-Campaz <dgcampaz@estudiante.uniajc.edu.co>
2026-02-10 16:30:32 +0000

    Se adicionó un elemento al array

--- Semana 2/ejercicio1.java
+++ Semana 2/ejercicio1.java
@@ -1,7 +1,9 @@
 public class ejercicio1 {
     public static void main(String[] args) {
+        
         //1st form: definiendo valores
-        int[] v = {1,4,5,7,9,2};
+        int[] v = {1, 4, 5, 7, 9, 2, 15};
+        
         //impresion
         for (int i = 0; i < v.length; i++){
             System.out.println("V["+i+"]= "+ v[i]);
